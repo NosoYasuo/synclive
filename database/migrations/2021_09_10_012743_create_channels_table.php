8842925753811c71b77688061c48a2f0b737b1b6
Soramahci <nosodayo@gmail.com>
2021-09-10 11:01:35 +0900

    add migration file

---
+++ database/migrations/2021_09_10_012743_create_channels_table.php
@@ -0,0 +1,49 @@
+
+    <?php
+        use Illuminate\Support\Facades\Schema;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Database\Migrations\Migration;
+        
+        class CreateChannelsTable extends Migration
+        {
+            /**
+             * Run the migrations.
+             *
+             * @return void
+             */
+            public function up()
+            {
+                Schema::create("channels", function (Blueprint $table) {
+
+						$table->bigIncrements('id');
+						$table->string('channel');
+						$table->bigInteger('users_id')->unsigned();
+						$table->timestamps();
+						//$table->foreign("users_id")->references("id")->on("users");
+
+
+
+						// ----------------------------------------------------
+						// -- SELECT [channels]--
+						// ----------------------------------------------------
+						// $query = DB::table("channels")
+						// ->leftJoin("users","users.id", "=", "channels.users_id")
+						// ->get();
+						// dd($query); //For checking
+
+
+
+                });
+            }
+
+            /**
+             * Reverse the migrations.
+             *
+             * @return void
+             */
+            public function down()
+            {
+                Schema::dropIfExists("channels");
+            }
+        }
+    
