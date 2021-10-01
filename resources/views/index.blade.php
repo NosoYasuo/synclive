31d5005fc869aea4aa95e724f30939b68f4aff2b
Soramahci <nosodayo@gmail.com>
2021-10-01 13:19:14 +0900

    change index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,53 +1,49 @@
 <!-- resources/views/books.blade.php -->
+
 @extends('layouts.app')
 @section('content')
 
+<link rel="stylesheet" type="text/css" href="css/styles.css">
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="{{ asset('/js/index.js') }}"></script>
 
-<h1 class="text-center">𝙎𝙮𝙣𝙘😄𝙇𝙞𝙫𝙚</h1>
-<h5 class="text-center">あなたも世界をライブ・プロデュース！💌</h5>
+<!-- watch ID登録フォーム -->
+<div class="sticky-top">
+<div class="flex container">
+<form action="{{ url('watch') }}" method="POST" class="form-inline">
+    {{ csrf_field() }}
+    <div class="form-group mb-2">
+      <div class="col-sm-6">
+        Watch ID<input type="text" name="watch_id" class="form-control">
+      </div>
+    </div>
+
+    <!-- 本 登録ボタン -->
+    <div class="form-group">
+    <div class="col-sm-offset-3 col-sm-6">
+      <button type="submit" class="btn btn-secondary">sync</button>
+    </div>
+</div>
+
+  </form>
+<div class="p-3 mb-2 .bg-gradient-dark bg-secondary text-white">
+<h2 class="text-center">𝙎𝙮𝙣𝙘𝙇𝙞𝙫𝙚</h2>
+<h6 class="text-center">あなたも世界のライブクリエーター！</h6>
+</div>
+</div>
+</div>
+
+
 
 <!-- バリデーションエラーの表示に使用-->
 @include('common.errors')
 <!-- バリデーションエラーの表示に使用-->
 
 <!-- 現在の本 -->
-@if (count($channels) > 0)
-<div>Channel一覧</div>
-<div class="card-body d-flex justify-content-around flex-wrap">
-@foreach ($channels as $channel)
-  <div>
-    <!-- 本タイトル -->
-
-    <!-- ?autoplay=1&mute=1&playsinline=1&loop=1 -->
-    <iframe width="373" height="210" src="https://www.youtube.com/embed/{{$channel->GetId($channel->channel)}}?autoplay=1&mute=1&playsinline=1&loop=1" frameborder="0" allowfullscreen></iframe>
-
-    <div style="display:none;">watch:<a id="c_watchId{{$channel->id}}">{{$channel->GetId($channel->channel)}}</a></div>
-    <div>No.{{$channel->id}}</div>
-    <div>作者:<a id="c_author{{$channel->id}}"></a></div>
-    <div>タイトル:<a id="c_title{{$channel->id}}"></a></div>
-    <div>users_id:{{$channel->users_id}}</div>
-    <div>created_at:{{$channel->created_at}}</div>
-
-    <!-- 本: 削除ボタン -->
-    <form action="{{ url('channel/'.$channel->id) }}" method="POST">
-      {{ csrf_field() }}
-      {{ method_field('delete') }}
-      <button type="submit" class="">
-        DELETE
-      </button>
-    </form>
-  </div>
-    <script>GetInfoChannel({{$channel->id}});</script>
-@endforeach
-</div>
-@endif
-<!-- Book: 既に登録されてる本のリスト -->
 
 <!-- 現在の本 -->
 @if (count($watches) > 0)
-<div>Watch一覧</div>
+{{-- <div>Watch一覧</div> --}}
 <div class="card-body d-flex justify-content-around flex-wrap">
   <!-- テーブル本体 -->
   @foreach ($watches as $watch)
@@ -56,9 +52,9 @@
 
     <!--autoplay=1&mute=1&playsinline=1&loop=1-->
     <iframe width="373" height="210" src="https://www.youtube.com/embed/{{$watch->watch}}?autoplay=1&mute=1&playsinline=1&loop=1&playlist={{$watch->watch}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <div>No.{{$watch->id}}</div>
-    <div>作者:<a id="author{{$watch->id}}"></a></div>
-    <div>タイトル:<a id="title{{$watch->id}}"></a></div>
+    <div>W# {{$watch->id}}</div>
+    <div>channel:<a id="author{{$watch->id}}"></a></div>
+    <div>title:<a id="title{{$watch->id}}"></a></div>
     <div style="display:none;">watch:<a id="watchId{{$watch->id}}">{{$watch->watch}}</a></div>
     <div>users_id:{{$watch->users_id}}</div>
     <div>created_at:{{$watch->created_at}}</div>
@@ -77,36 +73,57 @@
 </div>
 @endif
 <!-- 既に登録されてるリスト -->
+@if (count($channels) > 0)
+{{-- <div>Channel一覧</div> --}}
+<div class="card-body d-flex justify-content-around flex-wrap">
+@foreach ($channels as $channel)
+  <div>
+    <!-- 本タイトル -->
+
+    <!-- ?autoplay=1&mute=1&playsinline=1&loop=1 -->
+    <iframe width="373" height="210" src="https://www.youtube.com/embed/{{$channel->GetId($channel->channel)}}?autoplay=1&mute=1&playsinline=1&loop=1" frameborder="0" allowfullscreen></iframe>
+
+    <div style="display:none;">watch:<a id="c_watchId{{$channel->id}}">{{$channel->GetId($channel->channel)}}</a></div>
+    <div>C# {{$channel->id}}</div>
+    <div>channel:<a id="c_author{{$channel->id}}"></a></div>
+    <div>title:<a id="c_title{{$channel->id}}"></a></div>
+    <div>users_id:{{$channel->users_id}}</div>
+    <div>created_at:{{$channel->created_at}}</div>
+
+    <!-- 本: 削除ボタン -->
+    <form action="{{ url('channel/'.$channel->id) }}" method="POST">
+      {{ csrf_field() }}
+      {{ method_field('delete') }}
+      <button type="submit" class="">
+        DELETE
+      </button>
+    </form>
+  </div>
+    <script>GetInfoChannel({{$channel->id}});</script>
+@endforeach
+</div>
+@endif
+<!-- Book: 既に登録されてる本のリスト -->
 
 
 <!-- チャンネルID登録フォーム -->
-<form action="{{ url('post') }}" method="POST" class="form-horizontal">
+<div class="flex-container">
+<form action="{{ url('post') }}" method="POST" class="form-inline">
   {{ csrf_field() }}
-  <div>
-    <div>
-      チャンネルID<input type="text" name="channel_id" class="">
-    </div>
-  </div>
-  <!-- 本 登録ボタン -->
-  <div class="">
-    <div class="">
-      <button type="submit" class="">Save</button>
-    </div>
-  </div>
-</form>
 
-<!-- watch ID登録フォーム -->
-<form action="{{ url('watch') }}" method="POST" class="">
-  {{ csrf_field() }}
-  <div class="">
-    <div class="">
-      watchID<input type="text" name="watch_id" class="">
+    <div class="form-group mb-2">
+        <div class="col-sm-6">
+      Channel ID<input type="text" name="channel_id" class="form-control">
     </div>
-  </div>
+</div>
 
   <!-- 本 登録ボタン -->
-  <div class="">
-    <button type="submit" class="">Save</button>
+  <div class="form-group">
+    <div class="col-sm-offset-3 col-sm-6">
+      <button type="submit" class="btn btn-secondary">sync</button>
+    </div>
   </div>
+</div>
 </form>
+
 @endsection
