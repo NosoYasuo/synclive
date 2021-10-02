b8a5b57a867038f27ccc9b166bb4dd0312eff0c0
Soramahci <nosodayo@gmail.com>
2021-10-03 08:49:49 +0900

    change index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @extends('layouts.app')
 @section('content')
 
-<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="{{ asset('/js/index.js') }}"></script>
 
